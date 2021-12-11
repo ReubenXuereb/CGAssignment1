9cb78041761e52ba4fadbaa49bc5be0a5669f9dc
ReubenXuereb <56310716+ReubenXuereb@users.noreply.github.com>
2021-12-11 18:19:17 +0100

    trying to debug problem with username not being passed to databse

--- Assets/Scripts/StartButtonScript.cs
+++ Assets/Scripts/StartButtonScript.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Firebase.Database;
+using Firebase.Extensions;
+using UnityEngine.UI;
+using TMPro;
 
 public class StartButtonScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] GameObject usernameField;
+
+    string lobby = "1";
+    string username;
+
+    public void OnStartButton()
     {
-        
+        if (GetUsername())
+        {
+            FirebaseConfig firebase = new FirebaseConfig();
+            firebase.SendPlayerData(lobby, "1", new PlayerDetails(username, "square", "20", "riught now", "testing"));
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private bool GetUsername()
     {
-        
+        username = usernameField.transform.GetChild(0).GetComponent<TMP_InputField>().text;
+
+        if(username.Length >= 3)
+        {
+            print("Username valid");
+            return true;
+        }
+        else
+        {
+            print("username invalid");
+            return false;
+        }
     }
+ 
 }
